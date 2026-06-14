46ab09968d2262a62a2f4053fd02b07393f9c739
nhanpham-ptn <nhanpham.ptn2110@gmail.com>
2026-06-14 09:57:33 +0700

    Refactor makeStory function and file handling

--- Backend/Services/Story/Story.py
+++ Backend/Services/Story/Story.py
@@ -2,26 +2,24 @@ import json
 import requests
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
 load_dotenv()
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
-coterie = ""
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-books = [os.path.join(BASE_DIR, "LorePrompt.txt"), 
-         os.path.join(BASE_DIR, "20thAnni-world.txt"), 
-         os.path.join(BASE_DIR, "Disciplines.txt"), 
-         os.path.join(BASE_DIR, "KindredSociety.txt"), 
-         os.path.join(BASE_DIR, "Setting.txt")]
 
 
-for book in books:
-    prompt =  open(book, "r", encoding="utf-8");
-    coterie += "/////////// \n \n \n" + prompt.read()
-    prompt.close()
+def makeStory(coterie = None):
+    BASE_DIR = Path(__file__).resolve().parent
+    MATERIALS_DIR = BASE_DIR / "Materials"
+    coterie = ""
+
+    for book in MATERIALS_DIR.glob("*.txt"):
+        with open(book, "r", encoding="utf-8") as prompt:
+            coterie += "\n\n///////////\n\n" + prompt.read()
+            prompt.close()
 
 
-def makeStory(coterie):
     try:
         api_response = requests.post(
             url="https://openrouter.ai/api/v1/chat/completions",
@@ -39,21 +37,20 @@ def makeStory(coterie):
         api_json = api_response.json()
         assistant_msg = api_json['choices'][0]['message']['content']
 
-        return assistant_msg
+        def clean_text(text: str) -> str:
+            lines = text.splitlines()
+            cleaned = [line.strip() for line in lines if line.strip() != ""]
+            return "\n".join(cleaned)
+
+        if assistant_msg:
+            story = clean_text(assistant_msg)
+            with open("LoreSheet.txt", "w", encoding="utf-8") as lore:
+                lore.write(story + "\n\n")
+            
     except Exception as e:
-        print("Error" + e)
+        print(f"Error: {e}")
         return None
 
-story = makeStory(coterie)
 
-def clean_text(text: str) -> str:
-    lines = text.splitlines()
-    cleaned = [line.strip() for line in lines if line.strip() != ""]
-    return "\n".join(cleaned)
 
-if story:
-    story = clean_text(story)
-    lore_path = os.path.join(BASE_DIR, "LoreSheet.txt")
-    with open(lore_path, "w", encoding="utf-8") as lore:
-        lore.write(story + "\n\n")
         
